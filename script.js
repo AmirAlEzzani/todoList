aaff44ad364a6f9aa4dc227f705253f459f4d8d3
AmirAlEzzani <102375974+AmirAlEzzani@users.noreply.github.com>
2024-01-10 23:37:04 -0800

    js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 const ITEMS_CONTAINER = document.getElementById("items");
 const ITEMS_TEMPLATE = document.getElementById("itemTemplate");
 const ADD_BUTTON = document.getElementById("add");
+
+let items = getItems();
+
+function getItems() {
+    const value = localStorage.getItem("todo") || "[]";
+    return JSON.parse(value);
+}
+
+console.log(items);
