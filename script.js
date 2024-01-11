b5f655f93359af84280c962b50c1105e2a0e1e84
AmirAlEzzani <102375974+AmirAlEzzani@users.noreply.github.com>
2024-01-11 00:19:39 -0800

    completed

--- script.js
+++ script.js
@@ -9,4 +9,64 @@ function getItems() {
     return JSON.parse(value);
 }
 
-console.log(items);
+function setItems(items) {
+    const itemsJson = JSON.stringify(items);
+    localStorage.setItem("todo", itemsJson);
+}
+
+function addItem() {
+    items.unshift({
+        description: "",
+        completed: false
+    });
+
+    setItems(items);
+    refreshList();
+}
+
+function refreshList() {
+    items.sort((a,b) => {
+        if (a.completed) {
+            return 1;
+        }
+
+        if (b.completed) {
+            return -1;
+        }
+
+        return a.description < b.description ? -1 : 1;
+    });
+
+    ITEMS_CONTAINER.innerHTML = "";
+
+    for (const item of items) {
+        const itemElement = ITEMS_TEMPLATE.content.cloneNode(true);
+        const descriptionInput = itemElement.querySelector(".item-description");
+        const completedInput = itemElement.querySelector(".item-completed");
+
+        descriptionInput.value = item.description;
+        completedInput.checked = item.completed;
+
+        descriptionInput.addEventListener("change", () => {
+            updateItem(item, "description", descriptionInput.value);
+        });
+
+        completedInput.addEventListener("change", () => {
+            updateItem(item, "completed", completedInput.value);
+        });
+
+        ITEMS_CONTAINER.append(itemElement);
+    }
+}
+
+ADD_BUTTON.addEventListener("click", () => {
+    addItem();
+});
+
+function updateItem(item, key, value) {
+    item[key] = value;
+    setItems(items);
+    refreshList();
+}
+
+refreshList();
